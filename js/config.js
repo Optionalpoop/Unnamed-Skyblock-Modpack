163ece2b7fd13996ab925f902a9740b22e5ee653
Optionalpoop <90731028+Optionalpoop@users.noreply.github.com>
2026-01-18 09:40:09 -0500

    Add files via upload
    Initial Upload

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// Easy configuration file
+const PACK_NAME = "Untitled Skyblock Modpack";
+const VERSION = "0.0.1";
+
+
+// Apply values
+document.getElementById("pack-name").textContent = PACK_NAME;
+document.getElementById("version").textContent = "v" + VERSION;
